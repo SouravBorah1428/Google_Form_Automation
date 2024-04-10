bfb06cdbe0c0ad662723ef2b051805ad4875c817
SOURAV BORAH <souravborah1428@gmail.com>
2024-04-10 21:47:14 +0530

    code update

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,8 +1,13 @@
 package demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class TestCases {
@@ -25,9 +30,33 @@ public class TestCases {
 
     
     public  void testCase01(){
-        System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        System.out.println("Test Case 1: Single line input");
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+
+        WebElement nameField = driver.findElement(By.xpath("//div[@class='rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd']//input[@type='text']"));
+        nameField.sendKeys("Sourav Borah");
+    }
+
+    public void testCase02() {
+        System.out.println("Test Case 2: Multi line input");
+    }
+
+    public void testCase03() {
+        System.out.println("Test Case 3: Radio buttons");
+        WebElement radioButton = driver.findElement(By.xpath("//div[@class='SG0AAe']/child::div[1]"));
+        radioButton.click();
+    }
+
+    public void testCase04() {
+        System.out.println("Test Case 4: Checkboxes");
+
+        WebElement checkBox1 = driver.findElement(By.xpath("//div[@class='Y6Myld']//div[@role='list']/child::div[1]"));
+        WebElement checkBox2 = driver.findElement(By.xpath("//div[@class='Y6Myld']//div[@role='list']/child::div[2]"));
+        WebElement checkBox4 = driver.findElement(By.xpath("//div[@class='Y6Myld']//div[@role='list']/child::div[4]"));
+
+        checkBox1.click();
+        checkBox2.click();
+        checkBox4.click();
     }
 
 
